d06623dbbb6078424cd336419283e9bf715aa68f
Aaron Morrissey <amorrissey10@huskers.unl.edu>
2024-03-19 10:23:43 -0500

    Working on join game

--- client/src/pages/JoinGamePage.js
+++ client/src/pages/JoinGamePage.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/JoinGamePage.css";
 
 const JoinGamePage = () => {
-  return <div> Aaron Here</div>;
+  const [sessionId, setSessionId] = useState("");
+
+  const handleInputChange = (event) => {
+    setSessionId(event.target.value);
+  };
+
+  const handleJoinGame = () => {
+    console.log("Joining game with session ID:", sessionId);
+  };
+
+  return (
+    <div className="join-game-container">
+      <h1>Game Mode</h1>
+      <p>Join a game here:</p>
+      <input
+        type="text"
+        placeholder="Session ID or Enter Game Session ID"
+        value={sessionId}
+        onChange={handleInputChange}
+      />
+      <button className="join-button" onClick={handleJoinGame}>
+        Join Game
+      </button>
+    </div>
+  );
 };
 
 export default JoinGamePage;
